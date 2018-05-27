1dbf58c88b3fb918d7657bf93b900824b71630c2
Bernard Huff <bernard.huff008@gmail.com>
2018-05-27 16:53:15 -0500

    Added some code to manually create the storyboard for the project.

---
+++ CryptoTracker/CoinData.swift
@@ -0,0 +1,20 @@
+//
+//  CoinData.swift
+//  CryptoTracker
+//
+//  Created by Bernard Huff on 5/27/18.
+//  Copyright © 2018 Bernard Huff. All rights reserved.
+//
+
+import UIKit
+
+class Coin {
+    
+    var symbol = ""
+    var image = UIImage()
+    var price = 0.0
+    var ammount = 0.0
+    var historicalData = [Double]()
+    
+    
+}
